223ba8f7357b4189bcc2c1535c5455ea893e51a9
Haswinsingh <haswinsinghak@gmail.com>
2026-04-18 19:53:55 +0530

    fix api url

--- zynex-2026/src/api/register.js
+++ zynex-2026/src/api/register.js
@@ -1,4 +1,4 @@
-const API_URL = "https://zenvyxx.onrender.com";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const submitRegistration = async (formData) => {
   console.log("Registered VITE_API_URL:", import.meta.env.VITE_API_URL);
